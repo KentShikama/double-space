2462d8c032f60652654a65aa1335eb27905b03eb
Ziqi Xiong <zx012013@mymail.pomona.edu>
2015-01-19 15:15:00 -0800

    debug false

--- secret/settings.py
+++ secret/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '-%b2c9a3dhakm0(!ey9iys7x%%ac173@x$0rpajtd_*j^n)6(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
