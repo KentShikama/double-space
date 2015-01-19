47eba950dd5b1185bf1e65ab73f0cdb509d79673
Ziqi Xiong <zx012013@mymail.pomona.edu>
2015-01-19 15:33:50 -0800

    new database

--- secret/wsgi.py
+++ secret/wsgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "secret.settings")
 from whitenoise.django import DjangoWhiteNoise
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+application = Cling(application)
