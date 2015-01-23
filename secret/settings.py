21bed403336505bc250b834708414f92b5d1d2cb
theworldbright <sushionigiris@gmail.com>
2015-01-23 15:36:12 -0800

    Update slash

--- secret/settings.py
+++ secret/settings.py
@@ -93,7 +93,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/opt/myenv/static_doublespace'
+STATIC_ROOT = '/opt/myenv/static_doublespace/'
 
 DJANGO_WYSIWYG_FLAVOR = "ckeditor"
 
